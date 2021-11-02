922740ef91c418dcff26c87c779f9018b9c2f1bc
Aishna29 <93594504+Aishna29@users.noreply.github.com>
2021-11-02 23:20:10 +0530

    Update Brain.js

--- Brain.js
+++ Brain.js
@@ -1,13 +1,17 @@
-// This function will clear all the values
+// This function clear all the values
 function clearScreen() {
- document.getElementById("result").value = "";}
+    document.getElementById("answer").value = "";
+}
 
-// This function will display values
+
+// This function display values
 function display(value) {
- document.getElementById("result").value += value;}
+    document.getElementById("answer").value += value;
+}
 
-// This function will evaluates the expression and return result
+// This function evaluates the expression and return result
 function calculate() {
- var p = document.getElementById("result").value;
- var q = eval(p);
- document.getElementById("result").value = q;}
+    var p = document.getElementById("answer").value;
+    var q = eval(p);
+    document.getElementById("answer").value = q;
+}
